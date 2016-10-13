3d7eef05b3b510dff7b9bd66cf432aee4f88d858
jiraf <kikonshin@gmail.com>
2016-10-13 19:50:50 +0300

    edit

--- librip/decorators.py
+++ librip/decorators.py
@@ -4,23 +4,4 @@
 # Если функция вернула словарь (dict), то ключи и значения должны выводить в столбик через знак равно
 
 def print_result(func):
-    print(func.__name__)
-
-@print_result
-def test_1():
- return 1
-@print_result
-def test_2():
- return 'iu'
-@print_result
-def test_3():
- return {'a': 1, 'b': 2}
-@print_result
-def test_4():
- return [1, 2]
-
-test_1()
-test_2()
-test_3()
-test_4()
-
+	print(func.__name__)

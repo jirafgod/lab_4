b4323538517bec467fcc804d5e5ce1bad82178bc
jiraf <kikonshin@gmail.com>
2016-10-13 17:21:46 +0300

    )

--- ex_1.py
+++ ex_1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from librip.gens import field
+from librip.gen import *
+from librip.iterators import *
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
@@ -7,5 +8,7 @@ goods = [
     {'title': 'Стелаж', 'price': 7000, 'color': 'white'},
     {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
 ]
-
 # Реализация задания 1
+print(list(field(goods,'title','color')))
+print(list(gen_random(1,4,5)))
+
--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-from librip.gens import gen_random
+from librip.gen import gen_random
 from librip.iterators import Unique
 
-data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+data1 = [1,1,1,1,2,1,1,2,12,12,1,211]
 data2 = gen_random(1, 3, 10)
-
+data3 = ['K','O','S','T','Y','A','','g','o','d']
 # Реализация задания 2
+
+print(list(Unique(data1,ignore_case=True)))
+
+print(list(Unique(list(data2))))
+
+print(list(Unique(data3,ignore_case=True)))
+

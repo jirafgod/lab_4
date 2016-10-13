0a18fe9cff19eac8d876e72713e2768c065c8944
jiraf <kikonshin@gmail.com>
2016-10-13 17:22:02 +0300

    Add files via upload

--- librip/gen.py
+++ librip/gen.py
@@ -11,8 +11,19 @@ import random
 # field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
 
 def field(items, *args):
-    assert len(args) > 0
-    # Необходимо реализовать генератор 
+    lenargs = len(args)
+    assert lenargs > 0
+    # Необходимо реализовать генератор
+    for el in items:
+        buff = {}
+        if (lenargs==1):
+            if el[args[0]]!=None:
+                yield el[args[0]]
+        else:
+            for ar in args:
+                if el[ar] != None:
+                    buff[ar]=el[ar]
+            yield buff
 
 
 # Генератор списка случайных чисел
@@ -20,5 +31,6 @@ def field(items, *args):
 # gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
 # Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
+    for i in range(num_count):
+        yield random.randint(begin,end)
     # Необходимо реализовать генератор
--- librip/iterators.py
+++ librip/iterators.py
@@ -1,5 +1,7 @@
+from collections import Counter
 # Итератор для удаления дубликатов
 class Unique(object):
+    ignore_case=False
     def __init__(self, items, **kwargs):
         # Нужно реализовать конструктор
         # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
@@ -7,11 +9,21 @@ class Unique(object):
         # Например: ignore_case = True, Aбв и АБВ разные строки
         #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
         # По-умолчанию ignore_case = False
-        pass
-
+        self.ignore_case=False
+        if (len(kwargs)>0 and kwargs['ignore_case']!=None) and (type(kwargs['ignore_case'])==bool):
+            self.ignore_case=kwargs['ignore_case']
+        self.arr = items
+        self.buff=[]
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        for i in self.arr:
+            temp = str(i)
+            if (self.ignore_case):
+                temp = temp.lower()
+            if not temp in self.buff:
+                self.buff.append(temp)
+                return temp
+        raise StopIteration()
+
 
     def __iter__(self):
         return self

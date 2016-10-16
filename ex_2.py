1d44129f684a1ecd7b4e3ca39d82eec4668f26c3
jiraf <kikonshin@gmail.com>
2016-10-16 18:17:31 +0300

    Исправил немного

--- ex_2.py
+++ ex_2.py
@@ -2,14 +2,13 @@
 from librip.gen import gen_random
 from librip.iterators import Unique
 
-data1 = [1,1,1,1,2,1,1,2,12,12,1,211]
+data1 = [1, 1, 1, 1, 2, 1, 1, 2, 12, 12, 1, 211]
 data2 = gen_random(1, 3, 10)
-data3 = ['K','O','S','T','Y','A','','g','o','d']
+data3 = ['K', 'O', 'S', 'T', 'Y', 'A', '', 'g', 'o', 'd']
 # Реализация задания 2
 
-print(list(Unique(data1,ignore_case=True)))
+print(list(Unique(data1)))
 
 print(list(Unique(list(data2))))
 
-print(list(Unique(data3,ignore_case=True)))
-
+print(list(Unique(data3, ignore_case=True)))
